f2c80b37cae819f0461167cc2f9691862c803bcc
Norrick McGee <Norrick.McGee@gmail.com>
2018-10-18 21:01:24 -0800

    Added logic for line, pencil and rect,

--- paint.py
+++ paint.py
@@ -1,18 +1,22 @@
-import tkinter as tk 
+from tkinter import * 
 import tkinter.font as font
 
 #Class
 
 class painty:
 
+
+    ##### TO DO ######
+    #fix Oval, Text and Arc
+
 #Variables
 
-    drawing_tool = "line"
+    drawing_tool = "rectangle"
 
     mouse1 = "up"
 
-    xpos = None
-    ypos = None
+    x_pos = None
+    y_pos = None
 
     x1_point,y1_point,x2_point,y2_point = None, None, None, None
 
@@ -23,11 +27,19 @@ class painty:
         self.x_pos = None
         self.y_pos = None
 
-        self.x_point = event.x
-        self.y_point = event.y
+        self.x2_point = event.x
+        self.y2_point = event.y
 
         if self.drawing_tool == "line":
             self.line_draw(event)
+        elif self.drawing_tool == "arc":
+            self.arc_draw(event)
+        elif self.drawing_tool == "oval":
+            self.oval_draw(event)
+        elif self.drawing_tool == "rectangle":
+            self.rect_draw(event)
+        elif self.drawing_tool == "text":
+            self.text_draw(event)
         
 
 #mouse down
@@ -44,23 +56,45 @@ class painty:
 
         if self.drawing_tool == "pencil":
 
+            self.pencil_draw(event)           
+                
+#pencil draw
+    def pencil_draw(self, event = None):
+        if self.mouse1 == "down":
+
+
             if self.x_pos is not None and self.y_pos is not None:
 
                 event.widget.create_line(self.x_pos, self.y_pos, event.x, event.y, smooth = TRUE)
 
             self.x_pos = event.x
             self.y_pos = event.y
-            
-                
-#pencil draw
 
 #line draw
 
+    def line_draw(self, event=None):
+
+        if None not in (self.x1_point, self.y1_point, self.x2_point, self.y2_point):
+            event.widget.create_line(self.x1_point, self.y1_point, self.x2_point, self.y2_point, smooth = TRUE)
+
+
 #arc draw
+
+    def arc_draw(self, event = None):
+        if None not in (self.x1_point, self.y1_point, self.x2_point, self.y2_point):
+            event.widget.create_arc(self.x1_point, self.y1_point, self.x2_point, self.y2_point, smooth = TRUE)
 
 #oval draw
 
+    def oval_draw(self, event = None):
+        if None not in (self.x1_point, self.y1_point, self.x2_point, self.y2_point):
+            event.widget.create_oval(self.x1_point, self.y1_point, self.x2_point, self.y2_point, smooth = TRUE)
+
 #Rect draw
+
+    def rect_draw(self, event = None):
+        if None not in (self.x1_point, self.y1_point, self.x2_point, self.y2_point):
+            event.widget.create_rectangle(self.x1_point, self.y1_point, self.x2_point, self.y2_point)
 
 #Text draw
 
@@ -71,12 +105,12 @@ class painty:
 
         drawing_area.pack()
 
-        drawing.area.bind("<Motion>", self.motion)
-        drawing.area.bind("<ButtonPress-1>", self.m1_but_down)
-        drawing.area.bind("<ButtonRelease-1>", self.m1_but_up)
+        drawing_area.bind("<Motion>", self.motion)
+        drawing_area.bind("<ButtonPress-1>", self.m1_but_down)
+        drawing_area.bind("<ButtonRelease-1>", self.m1_but_up)
 
 root = Tk() 
-painty = PaintApp(root)
+Painty = painty(root)
 root.mainloop()
 
         
